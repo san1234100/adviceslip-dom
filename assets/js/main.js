e49ae68e213f2a23c7572541f910241249bcb853
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-02-19 18:41:38 +0530

    feat: generate random advice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,58 @@
-// Write your javascript code here
+// const person=new Promise((resolve,reject)=>{
+//     const name="";
+//    setTimeout(()=>{
+//     if(name!==''&&name!==null&&name.length>3){
+//         resolve(name);
+//      }else{
+//         reject("Enter valid name");
+//      }
+//    },3000)
+// })
+// // person.then((name)=>console.log(name))
+// // .catch((err)=>console.log("Error ",err))
+// // .finally(()=>console.log("Welcome to our program"))
+
+// const getResolution=async()=>{
+//     //await don't allow the code below this line until promise return the value
+
+//  try{
+//     const result=await person
+//     console.log(result);
+//  } catch(err){
+//     console.log('Error ',err);
+//  }
+
+//  console.log("Welcome to our program");
+// }
+// getResolution()
+const blockQuoteEl = document.querySelector("blockquote");
+const xhr = new XMLHttpRequest();
+xhr.responseType = "json";
+xhr.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    // const adviceObj=JSON.parse(xhr.responseText);
+
+    const adviceObj = xhr.response;
+
+    blockQuoteEl.innerText = adviceObj.slip.advice;
+  }
+};
+
+xhr.open("GET", "https://api.adviceslip.com/advice");
+xhr.send();
+
+// const result = fetch("https://api.adviceslip.com/advice");
+// result.then((res) => res.json())
+// .then((val) => val.slip.advice);
+
+async function getAdvices(){
+    try {
+        const result = await fetch("https://api.adviceslip.com/advice");
+        const json = await result.json();
+        console.log(json);
+      } catch (err) {
+        console.log(err);
+      }
+      
+}
+getAdvices()
